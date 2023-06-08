e1fb94f9a553f038a9f5161f6772ef91579fdf44
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-06-08 08:28:13 +0000

    fix(git): run install command after cloning

--- src/installers/_git.rs
+++ src/installers/_git.rs
@@ -1,6 +1,6 @@
 use anyhow::Error;
 use owo_colors::OwoColorize;
-use std::{any::Any, io::BufRead, process::Stdio};
+use std::{any::Any, io::BufRead, path::Path, process::Stdio};
 
 use crate::{macros::exec_bash_with_output, types::git::Repository};
 
@@ -78,48 +78,67 @@ impl Installer for GitInstaller {
         println!("-> 🚚 Installing {}", self.name().bright_green());
         self.preinstall()?;
 
-        let recursive = match self.recursive {
-            Some(recursive) => match recursive {
-                true => "--recursive",
-                false => "",
-            },
-            None => "",
-        };
+        let repo_dir = self.url.split("/").last().unwrap();
+        let repo_dir = repo_dir.split(".").next().unwrap();
 
-        let depth = match self.depth {
-            Some(depth) => format!("--depth {}", depth),
-            None => "".to_string(),
-        };
+        if !Path::exists(Path::new(repo_dir)) {
+            println!(
+                "-> Cloning {} into {}",
+                self.url.bright_green(),
+                repo_dir.bright_green()
+            );
+            let recursive = match self.recursive {
+                Some(recursive) => match recursive {
+                    true => "--recursive",
+                    false => "",
+                },
+                None => "",
+            };
 
-        let shallow_submodules = match self.shallow_submodules {
-            Some(shallow_submodules) => match shallow_submodules {
-                true => "--shallow-submodules",
-                false => "",
-            },
-            None => "",
-        };
+            let depth = match self.depth {
+                Some(depth) => format!("--depth {}", depth),
+                None => "".to_string(),
+            };
 
-        let git_clone = format!(
-            "git clone {} {} {} {}",
-            recursive, depth, shallow_submodules, self.url
+            let shallow_submodules = match self.shallow_submodules {
+                Some(shallow_submodules) => match shallow_submodules {
+                    true => "--shallow-submodules",
+                    false => "",
+                },
+                None => "",
+            };
+
+            let git_clone = format!(
+                "git clone {} {} {} {}",
+                recursive, depth, shallow_submodules, self.url
+            );
+
+            let mut child = std::process::Command::new("sh")
+                .arg("-c")
+                .arg(git_clone)
+                .stdout(std::process::Stdio::piped())
+                .stderr(std::process::Stdio::piped())
+                .spawn()?;
+            let output = child.stdout.take().unwrap();
+            let output = std::io::BufReader::new(output);
+            for line in output.lines() {
+                println!("{}", line?);
+            }
+            let status = child.wait()?;
+
+            if !status.success() {
+                return Err(anyhow::anyhow!("Failed to install {}", self.name()));
+            }
+        } else {
+            println!("-> {} already exists, skipping clone", repo_dir);
+        }
+
+        println!(
+            "-> Running install command: {}",
+            self.install.bright_green()
         );
 
-        let mut child = std::process::Command::new("sh")
-            .arg("-c")
-            .arg(git_clone)
-            .stdout(std::process::Stdio::piped())
-            .stderr(std::process::Stdio::piped())
-            .spawn()?;
-        let output = child.stdout.take().unwrap();
-        let output = std::io::BufReader::new(output);
-        for line in output.lines() {
-            println!("{}", line?);
-        }
-        let status = child.wait()?;
-
-        if !status.success() {
-            return Err(anyhow::anyhow!("Failed to install {}", self.name()));
-        }
+        exec_bash_with_output!(self.install.clone());
 
         self.postinstall()?;
         Ok(())
@@ -133,7 +152,7 @@ impl Installer for GitInstaller {
             );
             let home = std::env::var("HOME")?;
             let install = install.replace("~", &home);
-            if std::path::Path::new(&install).exists() {
+            if Path::new(&install).exists() {
                 return Ok(true);
             }
             return Err(anyhow::anyhow!("ble.sh is not installed"));
